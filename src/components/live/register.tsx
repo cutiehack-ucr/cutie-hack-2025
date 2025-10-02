00ad245148520bf802b3e2f15bc322d0f00f732f
unknown <twu148@ucr.edu>
2025-10-01 22:59:00 -0700

    feat: Change register link frames to on hover effect

--- src/components/live/register.tsx
+++ src/components/live/register.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import TitleBanner from "../ui/title-banner";
 
 const Register = () => {
+  const roles = ["participant", "judge", "mentor", "volunteer"];
+
   return (
     <div
       className="relative mx-auto flex w-5/6 flex-col items-center justify-center"
@@ -11,34 +13,31 @@ const Register = () => {
       <TitleBanner title="Register" />
 
       <div className="mt-10 grid max-w-4xl grid-cols-1 gap-x-12 gap-y-10 md:grid-cols-2 md:gap-y-16">
-        {[
-          { role: "participant", image: "/landing/participantFrame.webp" },
-          { role: "judge", image: "/landing/registerDecorLines.webp" },
-          { role: "mentor", image: "/landing/registerDecorLines.webp" },
-          { role: "volunteer", image: "/landing/registerDecorLines.webp" },
-        ].map(({ role, image }) => (
+        {roles.map((role) => (
           <Link
             key={role}
             href={role === "judge" ? "judge/register" : `apply/${role}`}
-            className={`flex transition-all ease-in-out hover:animate-pulse ${
-              role === "participant"
-                ? "justify-center"
-                : "flex-col items-center justify-center space-y-1"
-            }`}
+            className="group relative flex h-[80px] w-[250px] flex-col items-center justify-center transition-all ease-in-out hover:-translate-y-0.5 group-hover:flex-row"
           >
-            {role === "participant" && (
-              <Image alt="" width={250} height={0} src={image} aria-hidden />
-            )}
-            <p
-              className={`mt-4 font-inknut text-2xl text-hackathon-off-white-100 ${
-                role === "participant" ? "absolute" : ""
-              }`}
-            >
+            <Image
+              alt=""
+              width={225}
+              height={0}
+              src="/landing/registerDecorFrame.webp"
+              aria-hidden
+              className="hidden group-hover:block"
+            />
+            <p className="font-inknut text-2xl text-hackathon-off-white-100 group-hover:absolute group-hover:mb-6">
               {role.charAt(0).toUpperCase() + role.slice(1)}
             </p>
-            {role !== "participant" && (
-              <Image alt="" width={175} height={0} src={image} aria-hidden />
-            )}
+            <Image
+              alt=""
+              width={175}
+              height={0}
+              src="/landing/registerDecorLines.webp"
+              aria-hidden
+              className="group-hover:hidden"
+            />
           </Link>
         ))}
       </div>
