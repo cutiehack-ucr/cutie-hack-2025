5b91c0817ab3bf367352beb798133c36a730a79f
unknown <twu148@ucr.edu>
2025-10-01 21:55:55 -0700

    feat: Link judge registration

--- src/components/live/register.tsx
+++ src/components/live/register.tsx
@@ -8,9 +8,9 @@ const Register = () => {
       className="relative mx-auto flex w-5/6 flex-col items-center justify-center"
       id="register"
     >
-      <TitleBanner title="Register"/>
+      <TitleBanner title="Register" />
 
-      <div className="mt-10 grid max-w-4xl grid-cols-1 gap-y-10 md:grid-cols-2 gap-x-12 md:gap-y-16">
+      <div className="mt-10 grid max-w-4xl grid-cols-1 gap-x-12 gap-y-10 md:grid-cols-2 md:gap-y-16">
         {[
           { role: "participant", image: "/landing/participantFrame.webp" },
           { role: "judge", image: "/landing/registerDecorLines.webp" },
@@ -18,37 +18,27 @@ const Register = () => {
           { role: "volunteer", image: "/landing/registerDecorLines.webp" },
         ].map(({ role, image }) => (
           <Link
-        key={role}
-        href={`apply/${role}`}
-        className={`flex transition-all hover:animate-pulse ease-in-out ${
-          role === "participant" ? "justify-center" : "flex-col items-center justify-center space-y-1"
-        }`}
+            key={role}
+            href={role === "judge" ? "judge/register" : `apply/${role}`}
+            className={`flex transition-all ease-in-out hover:animate-pulse ${
+              role === "participant"
+                ? "justify-center"
+                : "flex-col items-center justify-center space-y-1"
+            }`}
           >
-        {role === "participant" && (
-          <Image
-            alt=""
-            width={250}
-            height={0}
-            src={image}
-            aria-hidden
-          />
-        )}
-        <p
-          className={`mt-4 font-inknut text-2xl text-hackathon-off-white-100 ${
-            role === "participant" ? "absolute" : ""
-          }`}
-        >
-          {role.charAt(0).toUpperCase() + role.slice(1)}
-        </p>
-        {role !== "participant" && (
-          <Image
-            alt=""
-            width={175}
-            height={0}
-            src={image}
-            aria-hidden
-          />
-        )}
+            {role === "participant" && (
+              <Image alt="" width={250} height={0} src={image} aria-hidden />
+            )}
+            <p
+              className={`mt-4 font-inknut text-2xl text-hackathon-off-white-100 ${
+                role === "participant" ? "absolute" : ""
+              }`}
+            >
+              {role.charAt(0).toUpperCase() + role.slice(1)}
+            </p>
+            {role !== "participant" && (
+              <Image alt="" width={175} height={0} src={image} aria-hidden />
+            )}
           </Link>
         ))}
       </div>
