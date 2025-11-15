9dd4027ad182582120a19be674c00ecb5e678f76
stanleylew5 <stanley43595@gmail.com>
2025-11-15 12:58:37 -0800

    add missing sponsor and judges

--- src/data/judges.ts
+++ src/data/judges.ts
@@ -4,27 +4,71 @@ import sushmalakshmi from "@/public/judges/sushmalakshmi.webp";
 import jerry from "@/public/judges/jerry.webp";
 import md from "@/public/judges/md.webp";
 import marios from "@/public/judges/marios.webp";
+import lisa from "@/public/judges/lisa.webp";
+import roshini from "@/public/judges/roshini.webp";
+import karan from "@/public/judges/karan.webp";
+import jonathan from "@/public/judges/jonathan.webp";
+import prithviraj from "@/public/judges/prithviraj.webp";
+import minnoli from "@/public/judges/minnoli.webp";
+import vahagn from "@/public/judges/vahagn.webp";
+import madhusudhan from "@/public/judges/madhusudhan.webp";
+import ashley from "@/public/judges/ashley.webp";
 
 const judges = [
-  {
-    name: "Ananya Dharanikota",
-    title: "Prev Terracon",
-    photo: ananya,
-  },
-  {
-    name: "Sarah Armstrong",
-    title: "Prev Givelify",
-    photo: sarah,
-  },
   {
     name: "Sushmalakshmi Velicheti",
     title: "Sr. Software Admin",
     photo: sushmalakshmi,
   },
   {
+    name: "Madhusudhan Tungamitta",
+    title: "Tech Staff @ Oracle",
+    photo: madhusudhan,
+  },
+  {
+    name: "Roshini Rangarajan",
+    title: "Prev @ HP",
+    photo: roshini,
+  },
+  {
+    name: "Ananya Dharanikota",
+    title: "Prev @ Terracon",
+    photo: ananya,
+  },
+  {
+    name: "Sarah Armstrong",
+    title: "Prev @ Givelify",
+    photo: sarah,
+  },
+  {
+    name: "Jonathan Darius",
+    title: "ML Researcher",
+    photo: jonathan,
+  },
+  {
+    name: "Prithviraj Yuvaraj",
+    title: "Professor",
+    photo: prithviraj,
+  },
+  {
+    name: "Ashley Pang",
+    title: "Associate Instructor",
+    photo: ashley,
+  },
+  {
     name: "Jerry Li",
     title: "ACM Board Alumni",
     photo: jerry,
+  },
+  {
+    name: "Karan Dhawan",
+    title: "Prev Citrus Lead",
+    photo: karan,
+  },
+  {
+    name: "Minnoli Nori",
+    title: "Prev Citrus Director",
+    photo: minnoli,
   },
   {
     name: "Md Abdullah Al Mamun",
@@ -35,6 +79,16 @@ const judges = [
     name: "Marios Nicolaides",
     title: "PhD Student",
     photo: marios,
+  },
+  {
+    name: "Vahagn Tovmasian",
+    title: "PhD Student",
+    photo: vahagn,
+  },
+  {
+    name: "Lisa Chen",
+    title: "Graduate Student",
+    photo: lisa,
   },
 ];
 export default judges;
